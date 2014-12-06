1b9eae4691be013f2949765f82c92127b9bc099b
Chesong Lee <KRUKUSA@gmail.com>
2014-12-06 15:31:10 -0800

    Trying to add menu
    Trying to add menu

--- HoneyBadgerApplication/gen/com/example/honeybadgerapplication/R.java
+++ HoneyBadgerApplication/gen/com/example/honeybadgerapplication/R.java
@@ -1647,11 +1647,12 @@ containing a value of this type.
         public static final int button_background_selector=0x7f02003a;
         public static final int cancelbut=0x7f02003b;
         public static final int confirmbut=0x7f02003c;
-        public static final int ic_launcher=0x7f02003d;
-        public static final int lg1=0x7f02003e;
-        public static final int line=0x7f02003f;
-        public static final int login=0x7f020040;
-        public static final int signup=0x7f020041;
+        public static final int ic_action_cancel=0x7f02003d;
+        public static final int ic_launcher=0x7f02003e;
+        public static final int lg1=0x7f02003f;
+        public static final int line=0x7f020040;
+        public static final int login=0x7f020041;
+        public static final int signup=0x7f020042;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f09003f;
@@ -1669,6 +1670,7 @@ containing a value of this type.
         public static final int action_bar_spinner=0x7f090002;
         public static final int action_bar_subtitle=0x7f09001f;
         public static final int action_bar_title=0x7f09001e;
+        public static final int action_cancel=0x7f09006b;
         public static final int action_context_bar=0x7f090032;
         public static final int action_menu_divider=0x7f090005;
         public static final int action_menu_presenter=0x7f090006;
@@ -1677,20 +1679,20 @@ containing a value of this type.
         public static final int action_mode_close_button=0x7f090020;
         public static final int action_settings=0x7f09006a;
         public static final int activity_chooser_view_content=0x7f090021;
-        public static final int address=0x7f09005c;
+        public static final int address=0x7f09005e;
         public static final int always=0x7f090014;
         public static final int amount=0x7f09004b;
         public static final int amountToTransfer=0x7f090040;
         public static final int amount_cred=0x7f09004e;
         public static final int beginning=0x7f09001b;
         public static final int birthday=0x7f090059;
-        public static final int cancelButton=0x7f090062;
+        public static final int cancelButton=0x7f090060;
         public static final int cancelButtonCreateAccountPage=0x7f09004d;
         public static final int cancelCloseAccount=0x7f090047;
         public static final int cancelTransfer=0x7f090045;
         public static final int cancel_cred=0x7f090052;
         public static final int checkbox=0x7f090029;
-        public static final int city=0x7f09005b;
+        public static final int city=0x7f09005c;
         public static final int closeAccount=0x7f090046;
         public static final int closeAccountButton=0x7f090068;
         public static final int collapseActionView=0x7f090015;
@@ -1702,10 +1704,10 @@ containing a value of this type.
         public static final int default_activity_button=0x7f090024;
         public static final int dialog=0x7f090019;
         public static final int disableHome=0x7f09000d;
-        public static final int doneButton=0x7f090063;
+        public static final int doneButton=0x7f09005f;
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090033;
-        public static final int email=0x7f09005f;
+        public static final int email=0x7f09005b;
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
@@ -1720,14 +1722,13 @@ containing a value of this type.
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090025;
         public static final int login=0x7f090058;
-        public static final int logout=0x7f09006b;
         public static final int middle=0x7f09001d;
         public static final int name=0x7f09005a;
         public static final int never=0x7f090017;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
         public static final int password=0x7f090054;
-        public static final int phone=0x7f090061;
+        public static final int phone=0x7f090062;
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
@@ -1749,7 +1750,7 @@ containing a value of this type.
         public static final int showTitle=0x7f090012;
         public static final int signUp=0x7f090056;
         public static final int split_action_bar=0x7f090004;
-        public static final int state=0x7f09005d;
+        public static final int state=0x7f090061;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
         public static final int title=0x7f090027;
@@ -1758,10 +1759,10 @@ containing a value of this type.
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
         public static final int username=0x7f090053;
-        public static final int verifyPassword=0x7f090060;
+        public static final int verifyPassword=0x7f090063;
         public static final int withText=0x7f090018;
         public static final int wrap_content=0x7f090009;
-        public static final int zip=0x7f09005e;
+        public static final int zip=0x7f09005d;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
@@ -1940,12 +1941,12 @@ containing a value of this type.
         public static final int title_activity_create_account=0x7f0a0039;
         public static final int title_activity_credit_debit=0x7f0a003a;
         public static final int title_activity_forgot_password=0x7f0a0038;
-        public static final int title_activity_reset_password=0x7f0a003f;
+        public static final int title_activity_reset_password=0x7f0a003e;
         /**  Android 
          */
         public static final int title_activity_sign_up=0x7f0a0036;
-        public static final int title_activity_teller_home_page=0x7f0a003e;
-        public static final int title_activity_user_home_page=0x7f0a003d;
+        public static final int title_activity_teller_home_page=0x7f0a003d;
+        public static final int title_activity_user_home_page=0x7f0a003f;
         /**  CloseAccount 
          */
         public static final int title_close_account_label=0x7f0a0035;
