08458be441bc6e8b6e2e7fc859a256936a6e27f4
Chesong Lee <KRUKUSA@gmail.com>
2014-12-07 16:10:33 -0800

    Splash screen
    Splash screen

---
+++ HoneyBadgerApplication/src/com/example/honeybadgerapplication/Splash.java
@@ -0,0 +1,32 @@
+package com.example.honeybadgerapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends ActionBarActivity {
+
+	private final int SPLASH_DISPLAY_LENGTH = 1000;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent mainIntent = new Intent(Splash.this, Login.class);
+				Splash.this.startActivity(mainIntent);
+				Splash.this.finish();
+			}
+		}, SPLASH_DISPLAY_LENGTH);
+	}
+
+
+}
